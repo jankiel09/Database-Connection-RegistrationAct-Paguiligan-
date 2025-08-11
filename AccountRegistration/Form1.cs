4643d0e4a02c51a7b2253ad3bbfaa617866a0c9e
jankiel09 <ezekielambat@gmail.com>
2025-08-11 19:40:37 +0800

    UI CREATED

--- AccountRegistration/Form1.cs
+++ AccountRegistration/Form1.cs
@@ -2,11 +2,14 @@ namespace AccountRegistration
 {
     public partial class Form1 : Form
     {
-
+        public class
         public Form1()
         {
             InitializeComponent();
         }
+
+
+
 
         private void textBox10_TextChanged(object sender, EventArgs e)
         {
